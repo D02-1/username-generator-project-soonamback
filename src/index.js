ff7687186f14815def8270af1fe71c240e7848e0
soonamback <soonam.yi123@gmail.com>
2021-09-23 11:03:19 +0200

    add createRandomNumber

--- src/index.js
+++ src/index.js
@@ -6,14 +6,27 @@ const path = require('path'); // Path, zum arbeiten mit dateipfaden
 
 // Was benötigen wir? 
 // Eine Funktion um die Werte aus der JSON datei zu lesen 
+/**
+ *  @function getWords
+ * @description reads words from json file
+ *  @returns { object }
+ */ 
+
 function getWords()
 {
     const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), 'utf-8'));
 
-return json 
+return json; 
 }
-getWords()
+
 // Ein Funktion um die zufällige Nummer hinter dem Namen zu generieren
+function createRandomNumber(minNumber, maxNumber)
+{
+    const num = Math.floor(Math.random() * (maxNumber - minNumber) + minNumber);
+
+    return num;
+}
+
 // Eine Funktion mit der wir den ersten Buchstaben des jeweiligen Wortes groß schreiben(PascalCase)
 // Eine Funktion mit der wir den Benutzernamen generieren  und auf die ergebnisse der anderen Funktion zugreifen
 // Einen Export, um die Applikation in der anderen Datei zu nutzen
